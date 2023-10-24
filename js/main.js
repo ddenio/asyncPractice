d78972e06fa5609565ee4ec3678c340d8bbd550d
Derin DeNio <derindenio@gmail.com>
2023-10-24 16:33:54 -0600

    Refactored jsonPromise

--- js/main.js
+++ js/main.js
@@ -2,14 +2,21 @@ const fetchPromise = fetch(
     "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
   );
   
-  console.log(fetchPromise);
-  
-  fetchPromise.then((response) => {
-    const jsonPromise = response.json();
-    jsonPromise.then((data) => {
+  fetchPromise
+    .then((response) => response.json())
+    .then((data) => {
       console.log(data[0].name);
-    });
-    console.log(`Received response: ${response.status}`);
-  });
+    })
+  
+  
+  // console.log(fetchPromise);
+  
+  // fetchPromise.then((response) => {
+  //   const jsonPromise = response.json();
+  //   jsonPromise.then((data) => {
+  //     console.log(data[0].name);
+  //   });
+  //   console.log(`Received response: ${response.status}`);
+  // });
   
   console.log("Started request…");
