81e9d057b98e8aed687d3acca01c410dbc7226ac
Derin DeNio <derindenio@gmail.com>
2023-10-24 18:04:09 -0600

    Added multiple promises and used the 'Promise.all()' method to catch any errors from any of the Promises

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-const fetchPromise = fetch(
-  "bad-scheme://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
-  );
+// const fetchPromise = fetch(
+//   "bad-scheme://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+//   );
   
-  fetchPromise
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data[0].name);
-    })
-    .catch((error) => {
-      console.error(`Could not get products: ${error}`);
-    });
+//   fetchPromise
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error(`HTTP error: ${response.status}`);
+//       }
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data[0].name);
+//     })
+//     .catch((error) => {
+//       console.error(`Could not get products: ${error}`);
+//     });
   
   
   // console.log(fetchPromise);
@@ -28,3 +28,23 @@ const fetchPromise = fetch(
   // });
   
   // console.log("Started request…");
+
+  const fetchPromise1 = fetch(
+    "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+  );
+  const fetchPromise2 = fetch(
+    "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found",
+  );
+  const fetchPromise3 = fetch(
+    "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json",
+  );
+  
+  Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
+    .then((responses) => {
+      for (const response of responses) {
+        console.log(`${response.url}: ${response.status}`);
+      }
+    })
+    .catch((error) => {
+      console.error(`Failed to fetch: ${error}`);
+    });
