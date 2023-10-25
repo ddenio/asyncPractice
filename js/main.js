1226996b474d938cbd7cea3678d0b64e3a3e79a3
Derin DeNio <derindenio@gmail.com>
2023-10-24 18:52:06 -0600

    Refactored old code to use Async/await

--- js/main.js
+++ js/main.js
@@ -29,42 +29,63 @@
 
 // console.log("Started request…");
 
-const fetchPromise1 = fetch(
-  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
-);
-const fetchPromise2 = fetch(
-  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found",
-);
-const fetchPromise3 = fetch(
-  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json",
-);
+// const fetchPromise1 = fetch(
+//   "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+// );
+// const fetchPromise2 = fetch(
+//   "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found",
+// );
+// const fetchPromise3 = fetch(
+//   "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json",
+// );
 
-Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
-  .then((responses) => {
-    for (const response of responses) {
-      console.log(`${response.url}: ${response.status}`);
+// Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
+//   .then((responses) => {
+//     for (const response of responses) {
+//       console.log(`${response.url}: ${response.status}`);
+//     }
+//   })
+//   .catch((error) => {
+//     console.error(`Failed to fetch: ${error}`);
+//   });
+
+// const fetchPromise4 = fetch(
+//   "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+// );
+// const fetchPromise5 = fetch(
+//   "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found",
+// );
+// const fetchPromise6 = fetch(
+//   "bad-scheme://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json",
+// );
+
+// Promise.all([fetchPromise4, fetchPromise5, fetchPromise6])
+//   .then((responses) => {
+//     for (const response of responses) {
+//       console.log(`${response.url}: ${response.status}`);
+//     }
+//   })
+//   .catch((error) => {
+//     console.error(`Failed to fetch: ${error}`);
+//   });
+
+async function fetchProducts() {
+  try {
+    // after this line, our function will wait for the `fetch()` call to be settled
+    // the `fetch()` call will either return a Response or throw an error
+    const response = await fetch(
+      "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
     }
-  })
-  .catch((error) => {
-    console.error(`Failed to fetch: ${error}`);
-  });
+    // after this line, our function will wait for the `response.json()` call to be settled
+    // the `response.json()` call will either return the parsed JSON object or throw an error
+    const data = await response.json();
+    console.log(data[0].name);
+  } catch (error) {
+    console.error(`Could not get products: ${error}`);
+  }
+}
 
-const fetchPromise4 = fetch(
-  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
-);
-const fetchPromise5 = fetch(
-  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found",
-);
-const fetchPromise6 = fetch(
-  "bad-scheme://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json",
-);
-
-Promise.all([fetchPromise4, fetchPromise5, fetchPromise6])
-  .then((responses) => {
-    for (const response of responses) {
-      console.log(`${response.url}: ${response.status}`);
-    }
-  })
-  .catch((error) => {
-    console.error(`Failed to fetch: ${error}`);
-  });
+fetchProducts();
